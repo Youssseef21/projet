049b919515df6ed548810a5699444c6797db75de
youssef <youssefharrane7@gmail.com>
2025-10-08 10:18:42 +0100

    hdzj

---
+++ src/main/java/tn/esprit/tpprojet/repositories/ProjetRepository.java
@@ -0,0 +1,11 @@
+package tn.esprit.tpprojet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.tpprojet.entity.Projet;
+
+@Repository
+public interface ProjetRepository extends JpaRepository<Projet, Long> {
+
+}
